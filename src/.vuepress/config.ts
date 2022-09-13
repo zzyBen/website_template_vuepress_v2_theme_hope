d58b9b71e3a0712d4d7ad133f48fcb190970a043
zzyBen <897224436@qq.com>
2022-09-13 00:42:46 -0500

    init theme hope

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,12 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  lang: "en-US",
+  title: "Theme Demo",
+  description: "A demo for vuepress-theme-hope",
+
+  base: "/website_template_vuepress_v2_theme_hope",
+
+  theme,
+});
